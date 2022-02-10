249a33dc7ce8c13cd8b0016a3577944562da8406
amyzhang <amyyzz08@gmail.com>
2022-02-10 10:54:58 +0800

    code so far

--- app.py
+++ app.py
@@ -2,12 +2,12 @@
 import datetime as dt
 import numpy as np
 
-# Database setup
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
+# Database Setup
 engine = create_engine("sqlite:///hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
@@ -26,15 +26,15 @@ def home():
     print("Wlecome to the Climate App Homepage")
     print("All available API routes are shown below:")
     return (
-        f"Precipitation:/precipitation<br/>"
-        f"Stations:/stations<br/>"
-        f"Temperature Observations:/tobs<br/>"
-        f"Temperature Summary for the start date(yyyy-mm-dd):/<start><br/>"
-        f"Temperature Summary for the selected period:/<start>/<end><br/>"
+        f"Precipitation:/api/v1.0/precipitation<br/>"
+        f"Stations:/api/v1.0/stations<br/>"
+        f"Temperature Observations for the most active station:/api/v1.0/tobs<br/>"
+        f"Temperature Summary for the start date(yyyy-mm-dd):/api/v1.0/<start><br/>"
+        f"Temperature Summary for the selected period:/api/v1.0/<start>/<end><br/>"
     )
 
 # Route to precipiation
-@app.route("/precipitation")
+@app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
     prec = [Measurement.date, Measurement.prec]
@@ -52,7 +52,7 @@ def precipitation():
     return jsonify(precipitation)
 
 # Route to stations
-@app.route("/stations")
+@app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
     sel = [Station.station,Station.name,Station.latitude,Station.longitude,Station.elevation]
@@ -74,29 +74,21 @@ def stations():
   
 
 # Route to List of Temperature Observations for most active station
-# @app.route("/tobs")
+# @app.route("/api/v1.0/tobs")
 # def temperature():
 #      session = Session(engine)
-#      active_station = session.query(Measurement.date, Measurement.tobs).\
-#                         filter(Measurement.date >= '2016-08-23').\
-#                         filter(Measurement.date <= '2017-08-23').\
-#                         filter(Measurement.station == 'USC00519281').all()
-#     sel = [Measurement.date, Measurement.tobs]
-#     station_query = session.query(*sel).filter(Measurement.station == 'USC00519281')
+#      start = '2016-08-23'
+#      sel =[Measurement.date, Measurement.tobs]
+#      active_station = 'USC00519281'
+#      station_tobs = session.query(*sel).\
+#                     filter(Measurement.date >= start, Measurement.station == active_station).\
+#                     group_by(Measurement.date).\
+#                     order_by(Measurement.date).all()
 #     session.close()
 
-#     tobs_json = []
-
-#     for date, tobs in station_query:
-#         tobs_dict = {}
-#         tobs_dict["Date"] = date
-#         tobs_dict["Tobs"] = tobs
-#         tobs_json.append(tobs_dict)
-
-#     return jsonify(tobs_json)
 
 # Route for TOBS for given Start Date
-@app.route("/<start>")
+@app.route("/api/v1.0/<start>")
 def start_date(start):
     session = Session(engine)
     start_result = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
@@ -106,15 +98,15 @@ def start_date(start):
     tobs_json = []
     for min,avg,max in start_result:
         tobs_dict = {}
-        tobs_dict["Min"] = min
-        tobs_dict["Average"] = avg
-        tobs_dict["Max"] = max
+        tobs_dict["Minimum Temperature"] = min
+        tobs_dict["Average Temperature"] = avg
+        tobs_dict["Maximum Temperature"] = max
         tobs_json.append(tobs_dict)
 
     return jsonify(tobs_json)
 
 # Route for TOBS for given start and end range
-@app.route("/<start>/<stop>")
+@app.route("/api/v1.0/<start>/<end>")
 def get_t_start_stop(start,end):
     session = Session(engine)
     query = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
@@ -124,9 +116,9 @@ def get_t_start_stop(start,end):
     tobs_json = []
     for min,avg,max in query:
         tobs_dict = {}
-        tobs_dict["Min"] = min
-        tobs_dict["Average"] = avg
-        tobs_dict["Max"] = max
+        tobs_dict["Minimum Temperature"] = min
+        tobs_dict["Average Temperature"] = avg
+        tobs_dict["aximum Temperature"] = max
         tobs_json.append(tobs_dict)
 
     return jsonify(tobs_json)
