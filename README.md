89a352c273d2f264c8c035c7c43573571942da52
amyzhang <amyyzz08@gmail.com>
2022-02-10 14:27:38 +0800

    challenge complete

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # **Sqlalchemy Challenge: Surfs Up!** 
 
+![](Images/surfs-up.png)
+
+<h2><b>Step 1 - Climate Analysis and Exploration</b></h2>
+
+The climate analysis and data exploration involved using Python, SQLAlchemy, ORM queries, Pandas and Matplotlib. 
+
+* SQLAlchemy <mark style="background-color: grey">create_engine</mark> is used to connect to the provided sqlite data base called <a href="Resources/hawaii.sqlite">hawaii.sqlite</a>
+
+* SQLAlchemy <mark style="background-color: grey">automap_base()</mark> is used to reflect the tables into classes and save a reference to those classes called <mark style="background-color: lightblue">Station</mark> and <mark style="background-color: lightblue">Measurement.</mark>
+
+<h3><b>Precipitation Analysis</b></h3>
+
+The query is to retrieve the last 12 months of precipitation data. The results were loaded into a Pandas DataFrame and visualised using the plot method.
+
+![](Images/precipitation.png)
+
+<h3><b>Station Analysis</b></h3>
+
+Several queries were designed to:
+
+1. Calculate the number of total stations
+
+2. Find the most active stations
+- The stations are listed in decsending order
+- The station with the highest number of observations is found
+- Functions such as <mark style="background-color: grey">func.min</mark>, <mark style="background-color: grey">func.max</mark>, <mark style="background-color: grey">func.avg</mark> and <mark style="background-color: grey">func.count</mark> are used in the queries
+3. To retrive the last 12 months of temperature observation data (TOBS)
+- The query is filtered by the station with the highest number of observations
+- The results are plotted as a histogram.
+
+![](Images/station-histogram.png)
+
+<h2><b>Step 2 - Climate App using Flask</b></h2>
+
+This task is to design a Flask application and create routes based on the queries from task 1.
+
+The queries will return in JSON format.
+
+<h3><b>API Routes</b></h3>
+<hr>
+
+<h4><b>1. Homepage</b></h4>
+
+![](Images/homepage.png)
+
+<h4><b>2. Precipitation</b></h4>
+
+![](Images/prcp.png)
+
+<h4><b>3. Stations</b></h4>
+
+![](Images/station.png)
+
+<h4><b>4. Temperature Observation Data of the most active Station</b></h4>
+
+![](Images/tobs.png)
+
+<h4><b>4. Temperature summary for a given start date</b></h4>
+
+![](Images/start.png)
+
+
+
+
+
+
+
--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 # Database Setup
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
@@ -23,31 +23,32 @@ app = Flask(__name__)
 # Route to Homepage
 @app.route("/")
 def home():
-    print("Wlecome to the Climate App Homepage")
-    print("All available API routes are shown below:")
     return (
-        f"Precipitation:/api/v1.0/precipitation<br/>"
-        f"Stations:/api/v1.0/stations<br/>"
-        f"Temperature Observations for the most active station:/api/v1.0/tobs<br/>"
-        f"Temperature Summary for the start date(yyyy-mm-dd):/api/v1.0/<start><br/>"
-        f"Temperature Summary for the selected period:/api/v1.0/<start>/<end><br/>"
+        f"<h1>Wlecome to the Climate App Homepage<h1/>"
+        f"<hr/>"
+        f"<h2>All available API routes are shown below:<h2/>"
+        f"<h3>Precipitation:<h3/><h5>/api/v1.0/precipitation<h5/>"
+        f"<h3>Stations:<h3/><h5>/api/v1.0/stations<h5/>"
+        f"<h3>Temperature Observations for the most active station:<h3/><h5>/api/v1.0/tobs<h5/>"
+        f"<h3>Temperature Summary for the start date(yyyy-mm-dd):<h3/><h5>/api/v1.0/start<h5/>"
+        f"<h3>Temperature Summary for the selected period:<h3/><h5>/api/v1.0/start_end<h5/>"
     )
 
 # Route to precipiation
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
-    prec = [Measurement.date, Measurement.prec]
-    prec_query = session.query(*prec).all()
+    prcp = [Measurement.date, Measurement.prcp]
+    prcp_query = session.query(*prcp).all()
     session.close()
 
     precipitation = []
 
-    for date, prec in prec_query:
-        prec_json = {}
-        prec_json["Date"] = date
-        prec_json["Precipitation"] = prec
-        precipitation.append(prec_json)
+    for date, prcp in prcp_query:
+        prcp_json = {}
+        prcp_json["Date"] = date
+        prcp_json["Precipitation"] = prcp
+        precipitation.append(prcp_json)
 
     return jsonify(precipitation)
 
@@ -72,25 +73,34 @@ def stations():
 
     return jsonify(stations)
   
-
 # Route to List of Temperature Observations for most active station
-# @app.route("/api/v1.0/tobs")
-# def temperature():
-#      session = Session(engine)
-#      start = '2016-08-23'
-#      sel =[Measurement.date, Measurement.tobs]
-#      active_station = 'USC00519281'
-#      station_tobs = session.query(*sel).\
-#                     filter(Measurement.date >= start, Measurement.station == active_station).\
-#                     group_by(Measurement.date).\
-#                     order_by(Measurement.date).all()
-#     session.close()
+@app.route("/api/v1.0/tobs")
+def temperature():
+    session = Session(engine)
+    start = '2016-08-23'
+    sel =[Measurement.date, Measurement.tobs]
+    active_station = 'USC00519281'
+    query_tobs = session.query(*sel).\
+                    filter(Measurement.date >= start, Measurement.station == active_station).\
+                    group_by(Measurement.date).\
+                    order_by(Measurement.date).all()
+    session.close()
 
+    tobs_station = []
+
+    for date, tobs in query_tobs:
+        tobs_dict = {}
+        tobs_dict["Date"] = date
+        tobs_dict["Tobs"] = tobs
+        tobs_station.append(tobs_dict)
+
+    return jsonify(tobs_station)
 
 # Route for TOBS for given Start Date
-@app.route("/api/v1.0/<start>")
-def start_date(start):
+@app.route("/api/v1.0/start")
+def start_date():
     session = Session(engine)
+    start = '2016-08-23'
     start_result = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
         filter(Measurement.date >= start).all()
     session.close()
@@ -106,9 +116,11 @@ def start_date(start):
     return jsonify(tobs_json)
 
 # Route for TOBS for given start and end range
-@app.route("/api/v1.0/<start>/<end>")
-def get_t_start_stop(start,end):
+@app.route("/api/v1.0/start_end")
+def get_t_start_stop():
     session = Session(engine)
+    start = '2016-08-23'
+    end = '2017-08-23'
     query = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
         filter(Measurement.date >= start).filter(Measurement.date <= end).all()
     session.close()
@@ -118,7 +130,7 @@ def get_t_start_stop(start,end):
         tobs_dict = {}
         tobs_dict["Minimum Temperature"] = min
         tobs_dict["Average Temperature"] = avg
-        tobs_dict["aximum Temperature"] = max
+        tobs_dict["Maximum Temperature"] = max
         tobs_json.append(tobs_dict)
 
     return jsonify(tobs_json)
